5816d2f0b947947f69f5d2a299f40f5e1cfbc330
Rani Saad <rsaad@live.ca>
2021-02-28 16:26:32 -0500

    Created fight function, console log test, playerName variable

---
+++ assets/js/game.js
@@ -0,0 +1,20 @@
+/* Created variable and used window prompt */
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+//A console log test
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+console.log("Bloop the Robot " + "is ready to fight!");
+console.log(playerName + " is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!");
+
+/*created new function with an alert */
+function fight() {
+  window.alert("The fight has begun");
+}
+
+/*called the function */
+//fight();
+
+//this is also a comment, using two slashes
