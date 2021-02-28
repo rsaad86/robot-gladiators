f431a533cc3d402ac4a2254d22c5817ecdbf67ad
Rani Saad <rsaad@live.ca>
2021-02-28 16:53:47 -0500

    added player and enemy attack as well as health update message

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,56 @@
 /* Created variable and used window prompt */
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-//A console log test
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
-console.log("Bloop the Robot " + "is ready to fight!");
-console.log(playerName + " is ready for battle!");
-console.log("Your robot, " + playerName + ", has won!");
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Robot Face";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log(enemyName, enemyAttack, enemyHealth);
+
+var fight = function () {
+  window.alert("Welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      ". " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining."
+  );
+
+  //check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
+
+  //check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
 
 /*created new function with an alert */
 function fight() {
@@ -15,6 +58,13 @@ function fight() {
 }
 
 /*called the function */
-//fight();
+fight();
 
 //this is also a comment, using two slashes
+/*A console log test
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+console.log("Bloop the Robot " + "is ready to fight!");
+console.log(playerName + " is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!");
+*/
